f48d893b015d28688e0f8a818a61d870069a16ed
Xargin26 <renlic77@live.cn>
2021-01-05 08:44:56 +0800

    add xmlhelper

--- Assets/Scripts/RecordScore.cs
+++ Assets/Scripts/RecordScore.cs
@@ -1,6 +1,8 @@
+using Assets.Scripts;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,7 +10,14 @@ public class RecordScore : MonoBehaviour
 {
     // Start is called before the first frame update
     public InputField _input;
+    public Text _score;
     public GameObject _playerScorePanel;
+    List<ScoreRow> _scoreTable;
+
+    void Start()
+    {
+        _scoreTable = XmlHelper.LoadScoreTable();
+    }
 
     public void OnClick()
     {
@@ -18,6 +27,34 @@ public class RecordScore : MonoBehaviour
         }
         else
         {
+            int score = int.Parse(_score.text);
+            string playerName = _input.text;
+            var scoreRow = new ScoreRow(playerName, score);
+            if (_scoreTable.Count < 10)
+            {
+                _scoreTable.Add(scoreRow);
+                _scoreTable.OrderByDescending(p => p.Score);
+                for (int i = 0; i < _scoreTable.Count; i++)
+                {
+                    _scoreTable[i].Id = i + 1;
+                }
+            }
+            else
+            {
+                if(_scoreTable.Any(p=>p.Score < score))
+                {
+                    _scoreTable.RemoveAt(_scoreTable.Count - 1);
+                    _scoreTable.Add(scoreRow);
+                    _scoreTable.OrderByDescending(p => p.Score);
+                    for (int i = 0; i < _scoreTable.Count; i++)
+                    {
+                        _scoreTable[i].Id = i + 1;
+                    }
+                }
+            }
+
+            XmlHelper.SaveScoreTable(_scoreTable);
+
             this.transform.parent.gameObject.SetActive(false);
             _playerScorePanel.SetActive(true);
         }
---
+++ Assets/Scripts/ScoreRow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts
+{
+    public class ScoreRow
+    {
+        public ScoreRow()
+        {
+
+        }
+
+        public ScoreRow(string playerName, int score)
+        {
+            PlayerName = playerName;
+            Score = score;
+            PlayTime = DateTime.Now;
+        }
+        public int Id { get; set; }
+
+        public string PlayerName { get; set; }
+
+        public int Score { get; set; }
+
+        public DateTime PlayTime { get; set; }
+    }
+}
